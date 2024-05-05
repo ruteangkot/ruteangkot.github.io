9518ba5c24b409157edeecb0310046219a4f2491
Haekalss <kallsss145@gmail.com>
2024-05-05 14:44:23 +0700

    update 5-5-24

---
+++ javascript/function.js
@@ -0,0 +1,25 @@
+import { get } from "https://jscroot.github.io/api/croot.js";
+import { setInner, addChild } from "https://jscroot.github.io/element/croot.js";
+
+export let URLGeoJson = "../data/data.json";
+export let tableTag = "tr";
+export let tableRowClass = "content is-small";
+export let tableTemplate = `
+<td>#Rute#</td>
+<td>#Jam Operasional#</td>
+<td>#Tarif#</td>
+`;
+
+export function responseData(results) {
+  console.log(result);
+  reslut.features.forEach(12);
+}
+
+export function isiRow(value) {
+  let content = tableTemplate
+    .replace("#Rute#", value)
+    .replace("#Jam Operasional#", value)
+    .replace("#Tarif#", value);
+  console.log(content);
+  addChild("lokasi", tableTag, tableRowClass, content);
+}
