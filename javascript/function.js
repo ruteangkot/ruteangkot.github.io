e6bd212ba507b142ecdaab6515b7daf7b4b01160
Haekalss <kallsss145@gmail.com>
2024-05-13 23:25:09 +0700

    update lagi

--- javascript/function.js
+++ javascript/function.js
@@ -43,13 +43,13 @@ function cari() {
   tr = table.getElementsByTagName("tr");
 
   for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0]; // Mengambil kolom pertama untuk pencarian
+    td = tr[i].getElementsByTagName("td")[0];
     if (td) {
       txtValue = td.textContent || td.innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = ""; // Tampilkan baris jika cocok dengan pencarian
+        tr[i].style.display = "";
       } else {
-        tr[i].style.display = "none"; // Sembunyikan baris jika tidak cocok
+        tr[i].style.display = "none";
       }
     }
   }
