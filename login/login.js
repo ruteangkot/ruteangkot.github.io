24be75dff73fa524f67ec30dfbe63caec5e21b70
Haekalss <kallsss145@gmail.com>
2024-06-26 19:22:52 +0700

    Frontend ||| update fungsi login

--- login/login.js
+++ login/login.js
@@ -6,19 +6,25 @@ document
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
+    const loginDetail = {
+      username: username,
+      password: password,
+    };
+
     const response = await fetch("/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ username, password }),
+      body: JSON.stringify(loginDetail),
     });
 
     const message = document.getElementById("message");
     if (response.status === 200) {
       message.textContent = "Login successful! Redirecting...";
       setTimeout(() => {
-        window.location.href = "home.html"; // Redirect to the home page or another page after login
+        window.location.href = "https://ruteangkot.github.io/admin/";
+        login;
       }, 2000);
     } else {
       const errorData = await response.json();
