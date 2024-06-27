b2a005d37290238d7e4669351407c97ffad9074c
RexndleZero <raihanaditya1506@gmail.com>
2024-06-27 10:16:54 +0700

    Frontend||| update di login dan register

--- login/login.js
+++ login/login.js
@@ -13,11 +13,11 @@ document
   .addEventListener("submit", async function (event) {
     event.preventDefault();
 
-    const username = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
     const loginDetail = {
-      username: username,
+      username: email,
       password: password,
     };
 
--- register/register.js
+++ register/register.js
@@ -13,6 +13,7 @@ document
     event.preventDefault();
 
     const username = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
     const confirmPassword = document.getElementById("confirm_password").value;
 
