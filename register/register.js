9647a0ee3efece0d41e76cc3743da4f64a1d457a
Haekalss <kallsss145@gmail.com>
2024-06-27 20:41:05 +0700

    menambahkan toggle visibilitas password

--- register/register.js
+++ register/register.js
@@ -1,47 +1,68 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
   const loginLink = document.getElementById("login-link");
 
-  loginLink.addEventListener("click", function (event) {
+  loginLink.addEventListener("click", (event) => {
     event.preventDefault();
-
     window.location.href = "https://ruteangkot.github.io/login/login.html";
   });
-});
 
-document
-  .getElementById("registerForm")
-  .addEventListener("submit", async function (event) {
-    event.preventDefault();
+  const togglePassword = document.getElementById("togglePassword");
+  const password = document.getElementById("password");
+  const toggleConfirmPassword = document.getElementById(
+    "toggleConfirmPassword"
+  );
+  const confirmPassword = document.getElementById("confirm_password");
 
-    const username = document.getElementById("username").value;
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    const confirmPassword = document.getElementById("confirm_password").value;
-
-    if (password !== confirmPassword) {
-      alert("Password do not match");
-      return;
-    }
-
-    const response = await fetch(
-      "https://asia-southeast2-awangga.cloudfunctions.net/ruteangkot/register",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ username, email, password }),
-      }
-    );
-
-    const message = document.getElementById("message");
-    if (response.status === 201) {
-      message.textContent = "Registration successful! Redirecting to login...";
-      setTimeout(() => {
-        window.location.href = "https://ruteangkot.github.io/admin";
-      }, 2000);
-    } else {
-      const errorData = await response.json();
-      message.textContent = `Error: ${errorData.error}`;
-    }
+  togglePassword.addEventListener("click", () => {
+    const type =
+      password.getAttribute("type") === "password" ? "text" : "password";
+    password.setAttribute("type", type);
+    togglePassword.textContent = type === "password" ? "Show" : "Hide";
   });
+
+  toggleConfirmPassword.addEventListener("click", () => {
+    const type =
+      confirmPassword.getAttribute("type") === "password" ? "text" : "password";
+    confirmPassword.setAttribute("type", type);
+    toggleConfirmPassword.textContent = type === "password" ? "Show" : "Hide";
+  });
+
+  document
+    .getElementById("registerForm")
+    .addEventListener("submit", async (event) => {
+      event.preventDefault();
+
+      const username = document.getElementById("username").value;
+      const email = document.getElementById("email").value;
+      const password = document.getElementById("password").value;
+      const confirmPassword = document.getElementById("confirm_password").value;
+
+      if (password !== confirmPassword) {
+        alert("Passwords do not match");
+        return;
+      }
+
+      const response = await fetch(
+        "https://asia-southeast2-awangga.cloudfunctions.net/ruteangkot/register",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ username, email, password }),
+        }
+      );
+
+      const message = document.getElementById("message");
+      if (response.status === 201) {
+        message.textContent =
+          "Registration successful! Redirecting to login...";
+        setTimeout(() => {
+          window.location.href = "https://ruteangkot.github.io/admin";
+        }, 2000);
+      } else {
+        const errorData = await response.json();
+        message.textContent = `Error: ${errorData.error}`;
+      }
+    });
+});
