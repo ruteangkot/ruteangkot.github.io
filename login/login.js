0f9d45b0889b472fc20ce7a03bf8c1d4062df391
RexndleZero <raihanaditya1506@gmail.com>
2024-07-08 21:05:04 +0700

    Frontend||| menghapus function reset password di login js

--- login/login.js
+++ login/login.js
@@ -3,10 +3,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const togglePassword = document.getElementById("togglePassword");
   const passwordField = document.getElementById("password");
   const message = document.getElementById("message");
-  const resetPasswordLink = document.getElementById("reset-password-link");
-  const resetPasswordModal = document.getElementById("resetPasswordModal");
-  const closeModal = document.querySelector(".close");
-  const resetPasswordForm = document.getElementById("resetPasswordForm");
 
   // Toggle Password Visibility
   togglePassword.addEventListener("click", () => {
@@ -60,42 +56,6 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
 
-  // Show Reset Password Modal
-  resetPasswordLink.addEventListener("click", (e) => {
-    e.preventDefault();
-    resetPasswordModal.style.display = "block";
-  });
-
-  // Close Modal
-  closeModal.addEventListener("click", () => {
-    resetPasswordModal.style.display = "none";
-  });
-
-  // Submit Reset Password Form
-  resetPasswordForm.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const email = document.getElementById("resetEmail").value;
-
-    try {
-      const response = await fetch(
-        "http://localhost:8080/request-reset-password",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ email }),
-        }
-      );
-
-      const data = await response.json();
-      alert(data.message);
-      resetPasswordModal.style.display = "none";
-    } catch (error) {
-      alert(`Error: ${error.message}`);
-      console.error("Error during password reset request:", error);
-    }
-  });
 
   // Redirect to Register Page
   loginLink.addEventListener("click", (event) => {
